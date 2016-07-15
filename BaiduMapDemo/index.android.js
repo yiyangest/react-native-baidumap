7929beb2b8f5d51ce22230b5aee4cec03904da1e
yiyang <y31210@gmail.com>
2016-07-15 18:09:36 +0800

    update to new version[android]

--- BaiduMapDemo/index.android.js
+++ BaiduMapDemo/index.android.js
@@ -12,20 +12,33 @@ import {
   View
 } from 'react-native';
 import BaiduMap from 'react-native-baidumap';
+import KKLocation from 'react-native-baidumap/KKLocation';
 
 class BaiduMapDemo extends Component {
+    componentDidMount() {
+        this.refs["mapView"].zoomToLocs([[39.832136, 116.34095]]);
+        KKLocation.getCurrentPosition((position) => {
+            console.log("location get current position: ", position);
+        }, (error) => {
+            console.log("location get current position error: ", error);
+        });
+        this.watchID = KKLocation.watchPosition((position) => {
+            console.log("watch position: ", position);
+        });
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-	<BaiduMap style={{flex: 1, width: 300}}
-	  showsUserLocation={true}
-          userLocationViewParams={{accuracyCircleFillColor: 'red', image: require('./start_icon.png')}}
-          annotations={[{latitude: 39.832136, longitude: 116.34095, title: "start", subtile: "hello", image: require('./stop_icon.png')}, {latitude: 39.902136, longitude: 116.44095, title: "end", subtile: "hello", image: require('./stop_icon.png')}]}
-          overlays={[{coordinates: [{latitude: 39.832136, longitude: 116.34095}, {latitude: 39.832136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.44095}], strokeColor: '#666666', lineWidth: 3}]}
-	/>
+          <Text style={styles.welcome}>
+              Welcome to React Native!
+          </Text>
+          <BaiduMap style={{flex: 1, width: 300}}
+              ref="mapView"
+              showsUserLocation={true}
+              userLocationViewParams={{accuracyCircleFillColor: 'red', image: require('./start_icon.png')}}
+              annotations={[{latitude: 39.832136, longitude: 116.34095, title: "start", subtile: "hello", image: require('./start_icon.png')}, {latitude: 39.902136, longitude: 116.44095, title: "end", subtile: "hello", image: require('./stop_icon.png')}]}
+              overlays={[{coordinates: [{latitude: 39.832136, longitude: 116.34095}, {latitude: 39.832136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.42095}, {latitude: 39.902136, longitude: 116.44095}], strokeColor: '#666666', lineWidth: 3}]}
+          />
       </View>
     );
   }
